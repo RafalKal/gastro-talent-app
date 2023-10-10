b3382d5daa091991cb585fdf3fbf34450755e875
Rafal_Kalandyk <rafal.kalandyk@asseco.pl>
2023-10-10 22:12:16 +0200

    project init

---
+++ src/main/java/com/java/gastrotalentapp/GastroTalentAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.gastrotalentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroTalentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastroTalentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/gastrotalentapp/GastroTalentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.gastrotalentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroTalentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
