2aaa6ac498915579e47a4634e4f37ee8744f81a3
NoobNobody <stasiu.karwala@gmail.com>
2023-10-27 01:49:59 +0200

    Role passing fixed

--- src/main/java/com/java/gastrotalentapp/controller/authController/AuthenticationController.java
+++ src/main/java/com/java/gastrotalentapp/controller/authController/AuthenticationController.java
@@ -3,15 +3,13 @@ package com.java.gastrotalentapp.controller.authController;
 import com.java.gastrotalentapp.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
+@CrossOrigin("*")
+public class    AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
--- src/main/java/com/java/gastrotalentapp/controller/authController/AuthenticationResponse.java
+++ src/main/java/com/java/gastrotalentapp/controller/authController/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.java.gastrotalentapp.controller.authController;
 
+import com.java.gastrotalentapp.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationResponse {
-
+    private Role role;
     private String token;
 
 }
